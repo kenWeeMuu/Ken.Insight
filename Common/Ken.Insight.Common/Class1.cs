6d264b04006c51704609f61c3b8253276f0cd10a
KenWeeMuu <ken@weemuu.com>
2015-11-28 19:39:02 +0800

    add my files

---
+++ Common/Ken.Insight.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ken.Insight.Common
+{
+    public class Class1
+    {
+    }
+}
